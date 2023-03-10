945c8f0847bf82040a1088211ca3dccde0b67e68
@jey927 <31284068+Jeniffer27@users.noreply.github.com>
2023-03-10 12:20:07 -0600

    Add files via upload

---
+++ FibonacciApp.swift
@@ -0,0 +1,17 @@
+//
+//  FibonacciApp.swift
+//  Fibonacci
+//
+//  Created by Jeniffer Calderón on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FibonacciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
